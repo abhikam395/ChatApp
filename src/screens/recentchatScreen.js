b34e6e6d80209edd742118690eea77d3e7b021d4
--add <--add>
2021-01-29 22:07:25 +0530

    ui bug removed

--- src/screens/recentchatScreen.js
+++ src/screens/recentchatScreen.js
@@ -13,7 +13,7 @@ export default class RecentChatScreen extends Component{
                 {id: 2, name: 'Rahul', lastmessage: 'Hello, How are you?', timestamp: '10 mins ago'},
                 {id: 3, name: 'Rahul', lastmessage: 'Hello, How are you?', timestamp: '10 mins ago'},
                 {id: 4, name: 'Rahul', lastmessage: 'Hello, How are you?', timestamp: '10 mins ago'},
-                {id: 5, name: 'Rahul', lastmessage: 'The box-shadow property enables you to cast a drop shadow from the frame of almost any element. If a border-radius is specified on the element with a box shadow, the box shadow takes on the same rounded corners. The z-ordering of multiple box shadows is the same as multiple text shadows (the first specified shadow is on top).', timestamp: '10 mins ago'},
+                {id: 5, name: 'Rahul', lastmessage: 'The box-shadow property enables you to cast a drop shadow from the frame of almost any element. If a border-radius is specified on the element with a box shadow, the box shadow takes on the same rounded corners. The z-ordering of multiple box shadows is the same as multiple text shadows (the first specified shadow is on top). The box-shadow property enables you to cast a drop shadow from the frame of almost any element. If a border-radius is specified on the element with a box shadow, the box shadow takes on the same rounded corners. The z-ordering of multiple box shadows is the same as multiple text shadows (the first specified shadow is on top).', timestamp: '10 mins ago'},
             ]
         }
     }
